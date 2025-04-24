4541b29f1bbbc3a1084d68830d72f60312360e6c
JVGorf <jv.gorf@gmail.com>
2025-04-23 20:56:46 -0700

    Add files via upload

---
+++ ga4.js
@@ -0,0 +1,9 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-C1SJN1Y6HY');
+
+var s = document.createElement('script');
+s.async = true;
+s.src = 'https://www.googletagmanager.com/gtag/js?id=G-C1SJN1Y6HY';
+document.head.appendChild(s);
